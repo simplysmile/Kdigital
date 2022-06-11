563081915b9cd2f40875c0a57dc66e5e1ac4e060
Grrrrrr <105529212+schreibmirnicht@users.noreply.github.com>
2022-06-11 13:17:13 +0900

    Fodo model 생성

--- Project/sChoice/AdminPage/models.py
+++ Project/sChoice/AdminPage/models.py
@@ -15,3 +15,18 @@ class Exercise(models.Model):
     muscle=models.CharField(max_length=1000,blank=True) # 자극되는 근육
     equipment=models.CharField(max_length=1000,blank=True) # 운동에 사용되는 운동기구
     imgUrl=models.ImageField(blank=True) # 이미지 주소
+    
+    
+    
+class Food(models.Model):
+    
+    f_NO = models.AutoField(primary_key=True) #음식번호
+    f_id = models.CharField(max_length=100,blank=True) #음식아이디
+    f_DB = models.CharField(max_length=100,blank=True) #음식DB군
+    f_name = models.CharField(max_length=100,blank=True) #음식이름
+    f_por = models.IntegerField(default=0,blank=True) #1회제공량 
+    f_carb = models.FloatField(default=0,blank=True) #탄수화물
+    f_protein = models.FloatField(default=0,blank=True) #단백질
+    f_fat = models.FloatField(default=0,blank=True) #지방
+    
+    
