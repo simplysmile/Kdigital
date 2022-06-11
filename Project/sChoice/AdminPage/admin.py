1920b0b445e8175130771f3f928ec1f5d8cbf842
Grrrrrr <105529212+schreibmirnicht@users.noreply.github.com>
2022-06-11 14:21:55 +0900

    FoodAdmin

--- Project/sChoice/AdminPage/admin.py
+++ Project/sChoice/AdminPage/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from AdminPage.models import Exercise
+from AdminPage.models import Exercise,Food
 
 @admin.register(Exercise)
 class ExerciseAdmin(admin.ModelAdmin):
     list_display=['ex_id','activity','aerobic','met','ex_name']
+    
+@admin.register(Food)
+
+class FoodAdmin(admin.ModelAdmin):
+    list_display=['f_NO','f_id','f_DB','f_name','f_por','f_carb','f_protein','f_fat']
 
 
