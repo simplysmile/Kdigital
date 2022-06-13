5b333f08429cde13e26f411ed57ff5a10b9fdbd3
Jihyeon <101442781+simplysmile@users.noreply.github.com>
2022-06-13 17:22:22 +0900

    행복 ㅎㅎㅎㅎ

--- Project/sChoice/DailyCheck/models.py
+++ Project/sChoice/DailyCheck/models.py
@@ -4,11 +4,11 @@ from AdminPage.models import Exercise
 from datetime import datetime
 
 class Dailyexercise(models.Model):
-    ex_No=models.AutoField(primary_key=True)
+    # ex_No=models.AutoField(primary_key=True)
     user=models.ForeignKey(Members,on_delete=models.CASCADE,null=True)
     exercise=models.ForeignKey(Exercise,on_delete=models.CASCADE,null=True) # 운동이름
     ex_time=models.IntegerField(default=0,blank=True) # 운동한 시간
     burned_kcal=models.IntegerField(default=0,blank=True) # 운동해서 태운 칼로리
     goal_kcal=models.IntegerField(default=1000,blank=True) # 내 운동 목표 칼로리
     content=models.TextField(blank=True)
-    createdate=models.DateTimeField(default=datetime.now(),blank=True) # 운동한 날짜
+    createdate=models.DateTimeField(auto_now_add=True,primary_key=True) # 운동한 날짜
