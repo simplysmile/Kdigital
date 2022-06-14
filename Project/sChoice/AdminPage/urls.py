e163155f0d9d6f586f93117ae538abc35aa6defc
Grrrrrr <105529212+schreibmirnicht@users.noreply.github.com>
2022-06-14 16:13:24 +0900

    admin update

--- Project/sChoice/AdminPage/urls.py
+++ Project/sChoice/AdminPage/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('',views.aboutus,name='index'),
     path('<str:searchword>/<str:category>/ad_m_L/',views.ad_m_L, name='ad_m_L'),
     path('<str:searchword>/<str:category>/<str:user_id>/ad_m_V/',views.ad_m_V, name='ad_m_V'),
+    path('<str:searchword>/<str:category>/<str:user_id>/ad_m_U/',views.ad_m_U, name='ad_m_U'),
     path('aboutus/',views.aboutus, name='aboutus'),
 ]
 
--- Project/sChoice/AdminPage/views.py
+++ Project/sChoice/AdminPage/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.shortcuts import render,redirect
 from Member.models import Members
 from django.db.models import Q
@@ -17,33 +18,82 @@ def ad_m_L(request,searchword,category):
 
     if category == 'user_name':
         qs = Members.objects.filter(user_name__contains=searchword).order_by('-user_name')
-        print('QQQQQQQQQQQQ:',qs)
+        
           
     elif category == 'user_id':
         qs = Members.objects.filter(user_id__contains=searchword).order_by('-user_name')
-        print('QQQQQQQQQQQQ:',qs)
+        
         
         
     elif category == 'email':
         qs = Members.objects.filter(email__contains=searchword).order_by('-user_name')
-        print('QQQQQQQQQQQQ:',qs)
+        
         
     else:
         qs = Members.objects.filter(Q(user_name__contains=searchword)|Q(user_id__contains=searchword)|Q(user_name__contains=searchword)).order_by('-user_name')
-        print('QQQQQQQQQQQQ:',qs)
+        
         
     context ={'admin_List':qs,'searchword':searchword,'category':category}
     
     return render(request,'ad_m_L.html',context)
 
 
+
+
+#어드민 뷰페이지
 def ad_m_V(request,user_id,searchword,category):
     
     qs = Members.objects.get(user_id=user_id)
     
     
     context ={'admin_List':qs,'searchword':searchword,'category':category}
-    print(context)
+    print('ccccccccccccccontext:',context)
     
 
     return render(request,'ad_m_V.html',context)
+
+
+#어드민 write페이지
+def ad_m_U(request,user_id,searchword,category):
+    
+    if request.method == 'GET':
+        qs = Members.objects.get(user_id=user_id)
+        context = {'ad_List':qs,'searchword':searchword,'category':category}
+        
+        return render(request,'ad_m_U.html',context)
+    
+    else:
+        qs = Members.objects.get(user_id=user_id)
+        
+        qs.user_name = request.POST.get('username')
+        qs.pro = request.POST.get('advancelevel')
+        
+        year  = int(request.POST.get('year'))
+        month = int(request.POST.get('month'))
+        day  = int(request.POST.get('day'))
+        
+        qs.birth = datetime.date(year,month,day)
+        qs.gender  = request.POST.get('gender')
+        qs.phone = request.POST.get('tel')
+        qs.email  = request.POST.get('email')
+        qs.zipcode = request.POST.get('postcode')
+        qs.addressd1 = request.POST.get('address')
+        qs.addressd2 = request.POST.get('detailAddress')
+        qs.addressd3 = request.POST.get('extraAddress')
+        qs.service= request.POST.get('service')
+        qs.user_purpose = request.POST.get('purpose')
+        qs.user_target = request.POST.get('target')
+        qs.vegan = request.POST.get('vegan')
+        qs.allergic_food  = request.POST.get('allergic_food')
+        qs.goal_weight = request.POST.get('goal_weight')
+        qs.goal_bodyfat= request.POST.get('goal_bodyfat')
+        qs.goal_period = request.POST.get('goal_period')
+        
+        qs.save()
+        
+        return redirect('AdminPage:ad_m_L',searchword,category)
+       
+        
+    
+    
+    
