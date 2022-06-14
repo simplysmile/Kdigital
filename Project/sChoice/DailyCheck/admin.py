b2196cb43fd33b95ad67be509db33f2efaec3a70
Grrrrrr <105529212+schreibmirnicht@users.noreply.github.com>
2022-06-14 11:07:42 +0900

    admin list display

--- Project/sChoice/DailyCheck/admin.py
+++ Project/sChoice/DailyCheck/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from DailyCheck.models import Dailyexercise
+from DailyCheck.models import Dailyexercise,DailyMeal
 
 @admin.register(Dailyexercise)
 class DailyexerciseAdmin(admin.ModelAdmin):
     list_display=['user','exercise','createdate']
+    
+    
+@admin.register(DailyMeal)
+class DailyMealAdmin(admin.ModelAdmin):
+    list_display=['d_member','d_food_name','d_por','d_carb','d_protein','d_fat','d_kcal']
 
 
