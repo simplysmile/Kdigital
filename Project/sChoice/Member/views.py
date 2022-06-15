2c1c0070624269c4219cba9492a285a308289079
Grrrrrr <105529212+schreibmirnicht@users.noreply.github.com>
2022-06-15 16:01:38 +0900

    회원가입 html 수정

--- Project/sChoice/Member/views.py
+++ Project/sChoice/Member/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
-from Member.models import Members
+from Member.models import Members,Dailydata
+from django.contrib.auth.hashers import make_password
 import datetime 
 
 # 내 정보수정 함수
@@ -84,36 +85,44 @@ def signup(request):   #회원가입 페이지를 보여주기 위한 함수
 
     elif request.method == "POST":
         
-        user_name = request.POST.get('username')
-        user_id = request.POST.get('user_id')   
-        user_pw  = request.POST.get('user_pw')
-        re_pw  = request.POST.get('re_password')
-        pro = request.POST.get('advancelevel')
+        user_name = request.POST.get('username', None)
+        user_id = request.POST.get('user_id',None)   
+        user_pw  = request.POST.get('user_pw',None)
+        re_pw  = request.POST.get('re_password',None)
+        pro = request.POST.get('advancelevel',None)
         print('전문가여부:',pro)
         year  = int(request.POST.get('year'))
         month = int(request.POST.get('month'))
         day  = int(request.POST.get('day'))
         birth = datetime.date(year,month,day)
-        gender  = request.POST.get('gender')
-        phone = request.POST.get('tel')
-        email  = request.POST.get('email')
-        zipcode = request.POST.get('postcode')
-        addressd1 = request.POST.get('address')
-        addressd2 = request.POST.get('detailAddress')
-        addressd3 = request.POST.get('extraAddress')
-        service= request.POST.get('service')
-        user_purpose = request.POST.get('purpose')
-        user_target = request.POST.get('target')
-        vegan = request.POST.get('vegan')
-        allergic_food  = request.POST.get('allergic_food')
-        goal_weight = request.POST.get('goal_weight')
-        goal_bodyfat= request.POST.get('goal_bodyfat')
-        goal_period = request.POST.get('goal_period')
+        gender  = request.POST.get('gender',None)
+        phone = request.POST.get('tel',None)
+        email  = request.POST.get('email',None)
+        zipcode = request.POST.get('postcode',None)
+        addressd1 = request.POST.get('address',None)
+        addressd2 = request.POST.get('detailAddress',None)
+        addressd3 = request.POST.get('extraAddress',None)
+        service= request.POST.get('service',None)
+        user_purpose = request.POST.get('purpose',None)
+        user_target = request.POST.get('target',None)
+        vegan = request.POST.get('vegan',None)
+        allergic_food  = request.POST.get('allergic_food',None)
+        goal_weight = request.POST.get('goal_weight',None)
+        print('goal_weight:',goal_weight)
+        goal_bodyfat= request.POST.get('goal_bodyfat',None)
+        goal_period = request.POST.get('goal_period',None)
+        height = request.POST.get('height',None)
+        cur_weight = request.POST.get('weight',None)
         
         
-        user = Members(user_id =user_id, user_pw =user_pw,user_name=user_name,pro=pro,birth=birth,gender=gender,phone=phone,email=email,zipcode=zipcode,addressd1=addressd1,\
+        
+        user = Members(user_id =user_id, user_pw = make_password(user_pw), user_name=user_name,pro=pro,birth=birth,gender=gender,phone=phone,email=email,zipcode=zipcode,addressd1=addressd1,\
             addressd2 = addressd2, addressd3 = addressd3, service=service,user_purpose=user_purpose, user_target=user_target, vegan=vegan, allergic_food=allergic_food, goal_weight=goal_weight, goal_bodyfat=goal_bodyfat, goal_period= goal_period)
         
+        user2 = Dailydata(height = height,cur_weight=cur_weight)
         user.save()
+        user2.save()
+        
+        
             
         return redirect('/')
