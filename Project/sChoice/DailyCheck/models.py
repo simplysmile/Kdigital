b53233744ac010e769f4f405243d12f3d315d45c
Jihyeon <101442781+simplysmile@users.noreply.github.com>
2022-06-15 15:09:15 +0900

    눈물의 오라클 ㅠㅠㅠㅠㅠ

--- Project/sChoice/DailyCheck/models.py
+++ Project/sChoice/DailyCheck/models.py
@@ -2,26 +2,29 @@ from django.db import models
 from Member.models import Members
 from AdminPage.models import Exercise,Food
 from datetime import datetime
+from django.utils.timezone import now
 
 class Dailyexercise(models.Model):
-    # ex_No=models.AutoField(primary_key=True)
+    ex_No=models.AutoField(primary_key=True)
     user=models.ForeignKey(Members,on_delete=models.CASCADE,null=True)
     exercise=models.ForeignKey(Exercise,on_delete=models.CASCADE,null=True) # 운동이름
     ex_time=models.IntegerField(default=0,blank=True) # 운동한 시간
     burned_kcal=models.IntegerField(default=0,blank=True) # 운동해서 태운 칼로리
     goal_kcal=models.IntegerField(default=1000,blank=True) # 내 운동 목표 칼로리
     content=models.TextField(blank=True)
-    createdate=models.DateTimeField(auto_now_add=True,primary_key=True) # 운동한 날짜
+    createdate=models.DateTimeField(default=now,editable=False) # 운동한 날짜
     
 class DailyMeal(models.Model):
+    d_No=models.AutoField(primary_key=True)
     d_member = models.ForeignKey(Members,on_delete=models.DO_NOTHING,null=True) #member 외래키
-    d_meal_date = models.DateTimeField(default=datetime.now(),blank=True) #식사날짜
+    d_meal_date = models.DateTimeField(default=now,blank=True) #식사날짜
     mael_time_Choice=(('B','아침'),('L','점심'),('D','저녁'),('S','간식'))
     d_meal_time = models.CharField(max_length=10,choices=mael_time_Choice,null=True) #식사시간
-    d_food = models.ForeignKey(Food,on_delete=models.DO_NOTHING,null=True) #Food 외래키
+    d_food = models.CharField(max_length=100,blank=True) #음식이름
     d_food_name = models.CharField(max_length=100,blank=True)
     d_por = models.IntegerField(default=0,blank=True) #1회제공량 
     d_carb = models.FloatField(default=0,blank=True) #탄수화물
     d_protein = models.FloatField(default=0,blank=True) #단백질
     d_fat = models.FloatField(default=0,blank=True) #지방
     d_kcal=models.IntegerField(default=0,blank=True) #칼로리
+    d_img=models.ImageField(blank=True)
--- Project/sChoice/Member/models.py
+++ Project/sChoice/Member/models.py
@@ -1,6 +1,7 @@
 # from pyexpat import model
 from django.db import models
 from datetime import datetime, date
+from django.utils.timezone import now
 # from PIL import Image
 
 class Members(models.Model):
@@ -30,22 +31,25 @@ class Members(models.Model):
     goal_weight = models.IntegerField(default=55,null=True)
     goal_bodyfat = models.IntegerField(default=25,null=True)
     goal_period = models.IntegerField(default=30,null=True)
-    createdate=models.DateTimeField(default=datetime.now(),blank=True)
-    modidate=models.DateTimeField(default=datetime.now(),blank=True)
+    createdate=models.DateTimeField(default=now,null=True)
+    modidate=models.DateTimeField(default=now,null=True)
     
 class Dailydata(models.Model):
-    user = models.ForeignKey(Members,on_delete=models.CASCADE)
-    height = models.IntegerField(blank=True)
-    cur_weight = models.IntegerField(blank=True)
-    cur_bmi=models.IntegerField(blank=True)
-    cur_bodyfat = models.IntegerField(blank=True)
-    cur_neck = models.IntegerField(blank=True)
-    cur_waist = models.IntegerField(blank=True)
-    cur_hip = models.IntegerField(blank=True)
-    day_img = models.ImageField(blank=True)
+    day_no=models.AutoField(primary_key=True)
+    user = models.ForeignKey(Members,on_delete=models.CASCADE,null=True)
+    height = models.IntegerField(null=True)
+    cur_weight = models.IntegerField(null=True)
+    cur_bmi=models.IntegerField(null=True)
+    cur_bodyfat = models.IntegerField(null=True)
+    cur_neck = models.IntegerField(null=True)
+    cur_waist = models.IntegerField(null=True)
+    cur_hip = models.IntegerField(null=True)
+    day_img = models.ImageField(null=True)
     level_Choice=(('2','저강도 운동'),('3','중강도 운동'),('6','고강도 운동')) # 운동강도
-    ex_level = models.CharField(max_length=100,choices=level_Choice,blank=True)
-    week_ex = models.IntegerField(default=0,blank=True)
-    day_ex = models.IntegerField(default=0,blank=True)    
-    add_date=models.DateTimeField(auto_now_add=True)
+    ex_level = models.CharField(max_length=100,choices=level_Choice,null=True)
+    week_ex = models.IntegerField(default=0,null=True)
+    day_ex = models.IntegerField(default=0,null=True)    
+    add_date=models.DateTimeField(default=now,null=True)
+    goal_eat_kcal=models.IntegerField(default=1000,null=True)
+    goal_burn_kcal=models.IntegerField(default=0,null=True)
     
