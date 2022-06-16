2ec6f8703ea05eaf5861c3d8769b169473bc6f24
Jihyeon <101442781+simplysmile@users.noreply.github.com>
2022-06-16 11:51:55 +0900

    보드

--- Project/sChoice/Board/views.py
+++ Project/sChoice/Board/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from Member.models import Members
+from Board.models import ExerciseBoard,MealBoard
 
 def exboard(request):
-    return render(request,'infoTable.html')
+    qs = ExerciseBoard.objects.order_by('-b_Group')
+    context={'board_list':qs}
+    
+    
+    return render(request,'infoTable.html',context)
 
 def exwrite(request):
-    return render(request,'boardWrite.html')
+    if request.method=="GET":
+        return render(request,'boardWrite.html')
+    
+    u_id=request.session.seession_id
+    bmem=Members.objects.get(user_id=u_id)
+    bispro=bmem.pro
+    btitle=request.POST.get('title')
+    bcontent=request.POST.get('content')
+    bfile=request.FILES.get('multim',None)
+    
+    qs = ExerciseBoard(member=bmem,m_Pro=bispro,b_Title=btitle,b_Content=bcontent,b_File=bfile)
+    qs.save()
+    qs.b_Group=qs.b_No
+    qs.save()
+    return redirect('Board:exboard')
+    
