b787313977feaaa501f9321b938b5fe5b58967c6
hlchoi622 <etoiles2@naver.com>
2022-06-24 18:08:47 +0900

    글꼴변경

--- Project/sChoice/static/js/mystatus.js
+++ Project/sChoice/static/js/mystatus.js
@@ -186,46 +186,46 @@ $(function(){
             $('#usertotalstatus').html(statstr)
 
             var mealtxt = ''
-            mealtxt += data.username +'님 지금까지 식단목표를 '+mealpercentage+'% 달성중입니다.'
+            mealtxt +='<div style="font-size:23px;">'+data.username +'님 지금까지 식단목표를 <span style="color:red; ">'+mealpercentage+'</span>% 달성중입니다.'
             if (mealpercentage > 80){
-                mealtxt +='<br>너무 잘하고 계세요. 지금처럼 계속 꾸준히 해주시면 원하시는 목표를 금방 이루실거에요 :) <br> <br>'
+                mealtxt +='<br>지금처럼 계속 꾸준히 해주시면 원하시는 목표를 금방 이루실거에요 :) </div>'
             }
             else if (mealpercentage > 50){
-                mealtxt +='<br>원하시는 목표위해서 조금만 더 분발해주세요 :) 화이팅 ! <br> <br>'
+                mealtxt +='<br>원하시는 목표위해서 조금만 더 분발해주세요 :) 화이팅 ! </div>'
             }
             else{
-                mealtxt +='<br>혹시 너무 무리한 목표설정이 아니신가요? 회원정보 수정에서 목표일을 변경하실 수 있습니다-. :) <br><br>'
+                mealtxt +='<br>혹시 너무 무리한 목표설정이 아니신가요? 회원정보 수정에서 목표일을 변경하실 수 있습니다-. :) </div>'
 
             }
-            mealtxt+='<li>평균적으로 아침, 점심, 저녁, 간식을 '+ data.minfo.blds_ratio +'의 비율로 섭취하고 있으며,</li>'
-            mealtxt+='<li>하루평균 '+ data.minfo.avgDCal +'칼로리를 섭취하고 있어요</li>'
+            mealtxt+='<li>평균적으로 아침, 점심, 저녁, 간식을 <span style="color:red; ">'+ data.minfo.blds_ratio +'</span>의 비율로 섭취하고 있으며,</li>'
+            mealtxt+='<li>하루평균 <span style="color:red; ">'+ data.minfo.avgDCal +'</span>칼로리를 섭취하고 있어요</li>'
             if (data.minfo.avgDCal<1000){
                 mealtxt +='<ul  style="font-size:12px; text-align:left;"> 너무 적은 칼로리를 섭취하고 계시네요. 이렇게 계속 섭취하시면 건강을 잃을 수 있어요. <br>혹시 너무 무리한 목표설정이 아니신가요? 회원정보 수정에서 목표일을 변경하실 수 있습니다-. :) 천천히 건강하게 살을빼세요 </ul>'
             }
-            mealtxt+='<li>또한 평균적으로 탄단지를 '+ data.minfo.cpf_ratio +'의 비율로 섭취하고 있어요.</li>'
+            mealtxt+='<li>또한 평균적으로 탄단지를 <span style="color:red; ">'+ data.minfo.cpf_ratio +'</span>의 비율로 섭취하고 있어요.</li>'
             mealtxt+='<li>건강한 다이어트를 위한 탄단지의 비율은 5:3:2, 혹은 4:4:2 비율이에요.</li>'
-            mealtxt+='<li>하루평균 탄수화물 '+Math.round(data.minfo.avgCarb,2) +'g, 단백질 ' +Math.round(data.minfo.avgProt)+ 'g, 지방 '+ Math.round(data.minfo.avgFat)+'g을 섭취하고 있어요.</li>'
+            mealtxt+='<li>하루평균 탄수화물 <span style="color:red; ">'+Math.round(data.minfo.avgCarb,2) +'</span>g, 단백질 <span style="color:red; ">' +Math.round(data.minfo.avgProt)+ '</span>g, 지방 <span style="color:red; ">'+ Math.round(data.minfo.avgFat)+'</span>g을 섭취하고 있어요.</li>'
             if (data.minfo.avgProt<data.weight[0]*1){
-                mealtxt+='<li>건강한 다이어트를 위해서 단백질은 하루에 '+ data.weight[0]*1 +'g 이상을 섭취하셔야 해요. 단백질 섭취량을 조금 늘려주세요'
+                mealtxt+='<li>건강한 다이어트를 위해서 단백질은 하루에 <span style="color:red; ">'+ data.weight[0]*1 +'</span>g 이상을 섭취하셔야 해요. 단백질 섭취량을 조금 늘려주세요'
             }
             if (data.minfo.avgCarb<100){
                 mealtxt+='<li>건강한 다이어트를 위해서 탄수화물은 하루에 100g 이상을 섭취하셔야 해요. 안그러면 탈모와 같은 부작용이 올 수 있어요. 탄수화물 섭취를 좀 늘려주세요'
             }
             var exertxt = ''
-            exertxt += data.username +'님 지금까지 운동목표를 '+exerpercentage+'% 달성중입니다.'
+            exertxt +='<div style="font-size:23px;">'+ data.username +'님 지금까지 운동목표를 <span style="color:red; ">'+exerpercentage+'</span>% 달성중입니다.'
             if (exerpercentage > 80){
-                exertxt +='<br>너무 잘하고 계세요. 지금처럼 계속 꾸준히 해주시면 원하시는 목표를 금방 이루실거에요 :) <br> <br>'
+                exertxt +='<br>지금처럼 계속 꾸준히 해주시면 원하시는 목표를 금방 이루실거에요 :) </div>'
             }
             else if (exerpercentage > 50){
-                exertxt +='<br>원하시는 목표위해서 조금만 더 분발해주세요 :) 화이팅 ! <br> <br>'
+                exertxt +='<br>원하시는 목표위해서 조금만 더 분발해주세요 :) 화이팅 ! </div>'
             }
             else{
-                exertxt +='<br>혹시 너무 무리한 목표설정이 아니신가요? 회원정보 수정에서 목표일을 변경하실 수 있습니다.  :) <br><br>'
+                exertxt +='<br>혹시 너무 무리한 목표설정이 아니신가요? 회원정보 수정에서 목표일을 변경하실 수 있습니다.  :) </div>'
 
             }
 
-            exertxt +='<li>현재까지 유산소 운동 '+ data.einfo.aerobic[1]+'분, 근력운동 '+data.einfo.aerobic[3] +'분 하셨어요. </li>'
-            exertxt +='<li>그 결과 유산소 운동으로 '+ data.einfo.aerobic[4]+'칼로리를 소모하시고 근력운동으로 '+data.einfo.aerobic[5] +' 칼로리를 소모하셨어요. </li>'
+            exertxt +='<li>현재까지 유산소 운동 <span style="color:red; ">'+ data.einfo.aerobic[1]+'</span>분, 근력운동 <span style="color:red; ">'+data.einfo.aerobic[3] +'</span>분 하셨어요. </li>'
+            exertxt +='<li>그 결과 유산소 운동으로 <span style="color:red; ">'+ data.einfo.aerobic[4]+'</span>칼로리를 소모하시고 근력운동으로 <span style="color:red; ">'+data.einfo.aerobic[5] +'</span> 칼로리를 소모하셨어요. </li>'
             if (data.einfo.aerobic[0]>data.einfo.aerobic[2]){ // 유산소를 더 많이할 경우
 
                 exertxt +='<ul style="font-size:12px; text-align:left;"> 무산소 운동은 근육의 크기와 힘을 키우고 순발력을 증가켜줘요. 또한 몸을 탄탄하게 하며 탄력적으로 보일 수 있게 해주며 근육량 증가에 의한 기초대사량의 유지,증가로 다이어트시의 정체기극복과 빠진 체중을 유지하는데도 도움을 주기때문에 근력 운동을 더 해주는게 어떨까요?</ul>'
@@ -235,9 +235,9 @@ $(function(){
 
             }
             
-            exertxt +='<li>가장 많이하신 운동은 '+data.einfo.mostexer+'이네요. 좋아하는 운동을 자주해보세요 :)</li>'
-            exertxt +='<li>오늘 까지 '+data.workoutday+'일 중에 '+ data.einfo.exerwk.reduce((a, b) => a + b, 0)+'일을 운동하셨어요.</li>'
-            exertxt +='<li>요일별 운동현황은 월: '+data.einfo.exerwk[0]+'회, 화: '+data.einfo.exerwk[1]+'회, 수: '+data.einfo.exerwk[2]+'회, 목: '+data.einfo.exerwk[3]+'회, 금: '+data.einfo.exerwk[4]+'회, 토: '+data.einfo.exerwk[5]+'회, 일: '+data.einfo.exerwk[6]+'회 하셨네요 :)</li>'
+            exertxt +='<li>가장 많이하신 운동은 <span style="color:red; ">'+data.einfo.mostexer+'</span>이네요. 좋아하는 운동을 자주해보세요 :)</li>'
+            exertxt +='<li>오늘 까지 <span style="color:red; ">'+data.workoutday+'</span>일 중에 <span style="color:red; ">'+ data.einfo.exerwk.reduce((a, b) => a + b, 0)+'</span>일을 운동하셨어요.</li>'
+            exertxt +='<li>요일별 운동현황은 월: <span style="color:red; ">'+data.einfo.exerwk[0]+'</span>회, 화: <span style="color:red; ">'+data.einfo.exerwk[1]+'</span>회, 수: <span style="color:red; ">'+data.einfo.exerwk[2]+'</span>회, 목: <span style="color:red; ">'+data.einfo.exerwk[3]+'</span>회, 금: <span style="color:red; ">'+data.einfo.exerwk[4]+'</span>회, 토: <span style="color:red; ">'+data.einfo.exerwk[5]+'</span>회, 일: <span style="color:red; ">'+data.einfo.exerwk[6]+'</span>회 하셨네요 :)</li>'
             exertxt +='<li>간단한 걷기, 스트레칭이라도 매일 꾸준히 해주시는것이 좋아요 :) 앞으로도 힘내서 꾸준히 운동해주세요.</li>'
 
             
